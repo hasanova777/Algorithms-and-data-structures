028747768e6b8168b08edbd46bd3cfbe4d79e82d
hasanova777 <hasssanova.177@gmail.com>
2021-03-03 00:04:23 +0300

    Added task P1

---
+++ homeworks/1/P1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void print_prime_nanbers (int n) {
+	vector<int> primes;
+	int k = 0;
+	int p = 3;
+	primes.push_back(2);
+	while (k < n) {
+		bool prime = true;
+		for (int i : primes) {
+			if (p % i == 0) {
+				prime = false;
+				break;
+			}
+		}
+		if (prime) {
+			primes.push_back(p);
+			cout << primes[k] << " ";
+			k++;
+		}
+		p++;
+	}
+	primes.~vector();
+}
+
+int main() {
+	int n;
+	cin >> n;
+	print_prime_nanbers(n);
+}
