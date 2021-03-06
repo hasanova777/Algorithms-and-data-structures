92c6358e54aaa52162e4dc22c130887521ecaa92
hasanova777 <hasssanova.177@gmail.com>
2021-03-06 20:51:54 +0300

    Added P1.cpp

--- homeworks/1/P1.cpp
+++ homeworks/1/P1.cpp
@@ -1,33 +1,67 @@
 #include <iostream>
-#include <vector>
-
 using namespace std;
 
-void print_prime_nanbers (int n) {
-	vector<int> primes;
-	int k = 0;
-	int p = 3;
-	primes.push_back(2);
-	while (k < n) {
-		bool prime = true;
-		for (int i : primes) {
-			if (p % i == 0) {
-				prime = false;
-				break;
-			}
-		}
-		if (prime) {
-			primes.push_back(p);
-			cout << primes[k] << " ";
-			k++;
-		}
-		p++;
+struct elem {
+	int val;
+	elem* next;
+};
+
+elem* add(elem* head, int x) {
+	elem* p = new elem;
+	(*p).val = x;
+	p->next = head;
+	return p;
+}
+
+void print_list(elem* head) {
+	elem* p = head;
+	while (p != NULL) {
+		cout << p->val << " ";
+		p = p->next;
 	}
-	primes.~vector();
+	cout << endl;
+}
+
+elem* empty_list() {
+	return NULL;
+}
+
+bool isExist(elem* el, int n) {
+	elem* p = el;
+	while (p != NULL) {
+		if (n % p->val == 0) {
+			return true;
+		}
+		p = p->next;
+	}
+	return false;
+}
+
+elem* expand(elem* li) {
+	elem* new_list = empty_list();
+	while (li != NULL) {
+		new_list = add(new_list, li->val);
+		li = li->next;
+	}
+	return new_list;
 }
 
 int main() {
-	int n;
+	int n, count = 1;
 	cin >> n;
-	print_prime_nanbers(n);
+	if (n > 0) {
+		elem* list = empty_list();
+		int i = 2;
+		list = add(list, i);
+		while (count < n) {
+			i++;
+			if (!isExist(list, i)) {
+				list = add(list, i);
+				count++;
+			}
+		}
+		list = expand(list);
+		print_list(list);
+	}
+	return 0;
 }
